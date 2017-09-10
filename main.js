7d12aa9459b6df5f7517ff4fd6a38e98458f3a5c
sbaxter95 <sbaxter95@outlook.com>
2017-09-10 13:29:22 +0100

    Refactored the javascript to start to include advanced calculator implementation

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var total = 0;
-var operation = prompt('What operation would you like to use? a(ddition) s(ubtraction) m(ultiplication) d(ivision)');
-var num1 = prompt('Enter first number');
-var num2 = prompt('Enter second number');
-num1 = parseFloat(num1);
-num2 = parseFloat(num2);
-if (operation === 'a') {
+var type = prompt('Would you like the a(dvanced) or the s(imple) calculator?');
+
+if (type === 's') {
+	var operation = prompt('What operation would you like to use? a(ddition) s(ubtraction) m(ultiplication) d(ivision)');
+	var num1 = prompt('Enter first number');
+	var num2 = prompt('Enter second number');
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	if (operation === 'a') {
 	total = num1 + num2;
 } else if (operation === 's') {
 	total = num1 - num2;
@@ -13,4 +16,8 @@ if (operation === 'a') {
 } else {
 	total = num1 / num2;
 }
+} else {
+
+}
+
 alert(total);
